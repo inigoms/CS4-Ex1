54b8c1effad9ca3c82a27f3a76a63cefafb7fa2b
IMS888 <>
2022-09-08 16:27:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+	double recorderRange = 2.5;
+	  
+	  int keyboardRange = 5;
+	  int violinrange = 4;  
+	System.out.print("Object 1");
+  }
+}
