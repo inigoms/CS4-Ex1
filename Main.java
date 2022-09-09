082cf795b11fe0dfce75b3497327bf4dec988da2
inigoms <112992543+inigoms@users.noreply.github.com>
2022-09-09 11:06:26 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,24 @@
 class Main {
   public static void main(String[] args) {
+	String Object1 = "Recorder";
+	String Object2 = "Violin";
+	String Object3 = "Portable Keyboard";
 	double recorderRange = 2.5;
-	  
-	  int keyboardRange = 5;
-	  int violinrange = 4;  
-	System.out.print("Object 1");
+	String recorderType = "wind";
+	double violinRange = 4.5;  
+	String violinType = "string";
+	double keyboardRange = 5.5;
+	String keyboardType = "MIDI";
+	System.out.println("Object 1: " + Object1);
+	System.out.println("Instrument Type: " + recorderType);
+	System.out.println("Range: " + recorderRange + " octaves");
+	System.out.println("");
+	System.out.println("Object 2: " + Object2);
+	System.out.println("Instrument Type: " + violinType);
+	System.out.println("Range: " + violinRange + " octaves");
+	System.out.println("");
+	System.out.println("Object 3: " + Object3);
+	System.out.println("Instrument Type: " + keyboardType);
+	System.out.println("Range: " + keyboardRange + " octaves");
   }
 }
