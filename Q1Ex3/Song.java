6f00c7d97270c4abd4e2251221ff0063ac195c1e
inigoms <112992543+inigoms@users.noreply.github.com>
2022-09-23 11:37:51 +0800

    Create Song.java

---
+++ Q1Ex3/Song.java
@@ -0,0 +1,11 @@
+public class Song{
+	static String title;
+	static double duration;
+	static int yearComposed;
+	
+	public Song(String t, double d, int y){
+		title = t;
+		duration = d;
+		yearComposed = y;
+	}
+}
