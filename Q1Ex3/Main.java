73eb859fe5c6ccfca3c9dd0df60e16281a28272c
inigoms <112992543+inigoms@users.noreply.github.com>
2022-09-23 12:11:52 +0800

    Update Main.java

--- Q1Ex3/Main.java
+++ Q1Ex3/Main.java
@@ -1,20 +1,34 @@
 class Main {
 	public static void main(String[] args) {
-		Instrument keyboard = new instrument(5.5, "MIDI", "gold");
-		Instrument violin = new instrument(4, "string", "brown and black");
-		Instrument recorder = new instrument(2.5, "wind", "beige");
+		Instrument keyboard = new Instrument("keyboard", 5.5, "MIDI");
+		Instrument violin = new Instrument("violin", 4, "string");
+		Instrument recorder = new Instrument("recorder", 2.5, "wind");
 		Song piece1 = new Song("Bach - Agnus Dei", 6.5, 1749);
 		Song piece2 = new Song("Handel - Ombra mai fu", 3, 1738);
-		Singer Andreas = new Singer("Andreas Scholl", 0, 0, piece1, piece2);
-		System.out.println("Singer Name: " + Singer.getName());
-		System.out.println("No of Performances: " + Singer.getNoOfPerformances());
-		System.out.println("Earnings: " + Singer.getEarnings());
-		System.out.println("Favorite Song: " + Singer.getFavoriteSong().title);
+		Singer Andreas = new Singer("Andreas Scholl", 0, 0, piece1);
+		System.out.println("Instrument Name: " + keyboard.getIName());
+		System.out.println("   Range: " + keyboard.getIRange());
+		System.out.println("   Type: " + keyboard.getIType());
+		System.out.println("Instrument Name: " + violin.getIName());
+		System.out.println("   Range: " + violin.getIRange());
+		System.out.println("   Type: " + violin.getIType());
+		System.out.println("Instrument Name: " + recorder.getIName());
+		System.out.println("   Range: " + recorder.getIRange());
+		System.out.println("   Type: " + recorder.getIType());
+		System.out.println("");
+		System.out.println("Singer Name: " + Andreas.getName());
+		System.out.println("No of Performances: " + Andreas.getNoOfPerformances());
+		System.out.println("Earnings: " + Andreas.getEarnings());
+		System.out.println("Favorite Song: " + Andreas.getFavoriteSong().title);
+		System.out.println("   Duration: " + Andreas.getFavoriteSong().duration + " min");
+		System.out.println("   Year composed: " + Andreas.getFavoriteSong().yearComposed);
 		System.out.println("");
 		Andreas.performForAudience(12);
-		Andreas.change();
-		System.out.println("No of Performances: " + Singer.getNoOfPerformances());
-		System.out.println("Earnings: " + Singer.getEarnings());
-		System.out.println("Favorite Song: " + Singer.getFavoriteSong().title);
+		Andreas.change(piece1);
+		System.out.println("No of Performances: " + Andreas.getNoOfPerformances());
+		System.out.println("Earnings: " + Andreas.getEarnings());
+		System.out.println("Favorite Song: " + Andreas.getFavoriteSong().title);
+		System.out.println("   Duration: " + Andreas.getFavoriteSong().duration + " min");
+		System.out.println("   Year composed: " + Andreas.getFavoriteSong().yearComposed);
 	}
 }
