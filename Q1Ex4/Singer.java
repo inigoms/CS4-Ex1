a8ea55a253fedb374fa66d4f0e8446a7721e5114
inigoms <112992543+inigoms@users.noreply.github.com>
2022-09-30 11:42:18 +0800

    Create Singer.java

---
+++ Q1Ex4/Singer.java
@@ -0,0 +1,38 @@
+public class Singer{
+	String name;
+	int noOfPerformances;
+	double earnings;
+	Song favoriteSong;
+
+	public void performForAudience(int noOfPeople){
+		noOfPerformances += noOfPeople;
+		earnings += noOfPeople * 100;
+	}
+
+	public void changeFavSong(Song c){
+		favoriteSong = c;
+	}
+
+	public Singer(String n, int p, double e, Song f){
+		name = n;
+		noOfPerformances = p;
+		earnings = e;
+		favoriteSong = f;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	
+	public int getNoOfPerformances(){
+		return noOfPerformances;
+	}
+	
+	public double getEarnings(){
+		return earnings;
+	}
+	
+	public Song getFavoriteSong(){
+		return favoriteSong;
+	}
+}
