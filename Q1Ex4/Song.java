b564b945a3ff070e5b5bf1c23d0f68b87827d015
inigoms <112992543+inigoms@users.noreply.github.com>
2022-10-04 08:57:29 +0800

    Update Song.java

--- Q1Ex4/Song.java
+++ Q1Ex4/Song.java
@@ -1,11 +1,21 @@
 public class Song{
-	String title;
-	double duration;
-	int yearComposed;
+	private String title;
+	private double duration;
+	private int yearComposed;
 	
 	public Song(String t, double d, int y){
 		title = t;
 		duration = d;
 		yearComposed = y;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	public double getDuration(){
+		return duration;
+	}
+	public int getYearComposed(){
+		return yearComposed;
 	}
 }
